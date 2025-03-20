8b1572e6ae81c07b3b865a7e330e1b882ba65921
Jon Young <youngt.jonathan>
2025-03-20 11:39:28 -0500

    added field module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,24 @@
-console.log("Welcome to the main module")
+import { addPlant, usePlants } from "./field.js"
+import { createPlan } from "./plan.js"
 
+
+
+const yearlyPlan = createPlan()
+console.log(yearlyPlan)
+
+
+
+
+// passes certain seed objects into the usePlants array
+const addAsparagus = usePlants(asparagusSeed)
+console.log(addAsparagus)
+const addCorn = usePlants(cornSeed)
+console.log(addCorn)
+const addWheat = usePlants(wheatSeed)
+console.log(addWheat)
+const addAsparagus = usePlants(asparagusSeed)
+console.log(addAsparagus)
+const addAsparagus = usePlants(asparagusSeed)
+console.log(addAsparagus)
+const addAsparagus = usePlants(asparagusSeed)
+console.log(addAsparagus)
