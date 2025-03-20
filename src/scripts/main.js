a8dd735ff431b1ff3841f3908379a324c3020fed
Dominic Tillison <22tilldj@gmail.com>
2025-03-20 12:39:54 -0400

    iterated over plants array and returns createdSeeds function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,9 @@
+import { createPlan } from "./plan.js";
+import { plantSeeds } from "./tractor.js";
+const yearlyPlan = createPlan()
+
 console.log("Welcome to the main module")
+
+const plantedSeeds = plantSeeds(yearlyPlan)
+console.log(plantedSeeds)
 
