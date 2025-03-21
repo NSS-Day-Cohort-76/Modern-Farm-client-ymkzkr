b83523b58c3d951ebff0e531558e8e61cc6eebb0
Jon Young <youngt.jonathan>
2025-03-21 13:12:29 -0500

    added weather moduels

---
+++ src/scripts/applyWeather.js
@@ -0,0 +1,7 @@
+
+
+export const applyWeather = (harvestedFood, weather) => {
+for (let i = 0; i < harvestedFood.length; i++) {
+    harvestedFood[i].output *= weather.effect;
+}
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,22 @@ import { plantSeeds } from "./tractor.js";
 import {harvestPlants} from "./harvester.js"
 import {countPlants} from "./catalog.js";
 import {countPlantsHTML} from "./catalog.js";
+import { getWeather } from "./weather.js";
+import { applyWeather } from "./applyWeather.js";
 
 
-const yearlyPlan = createPlan()
+const yearlyPlan = createPlan();
 
-plantSeeds(yearlyPlan)
+plantSeeds(yearlyPlan);
 
-const plants = usePlants()
+const plants = usePlants();
 
 
-const harvestedFood = harvestPlants(plants)
-const allPlants = countPlants(harvestedFood)
-const orderPlants = countPlantsHTML(allPlants)
+const harvestedFood = harvestPlants(plants);
+const allPlants = countPlants(harvestedFood);
+const orderPlants = countPlantsHTML(allPlants);
+const weather = getWeather();
+applyWeather(harvestedFood, weather)
 
 const render = document.querySelector(".container")
 render.innerHTML = orderPlants
